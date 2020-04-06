66623ea48619d6ea425709d532d5114dfbe635ee
lanlin <1094506821@qq.com>
2020-04-06 08:14:14 +0800

    ..

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,18 +1,18 @@
 <template>
   <div>
     <el-breadcrumb separator-class="el-icon-arrow-right">
-      <el-breadcrumb-item :to="{ path: '/Home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item :to="{ name: 'AdminIndex' }">首页</el-breadcrumb-item>
       <el-breadcrumb-item>用户配置</el-breadcrumb-item>
       <el-breadcrumb-item>用户管理</el-breadcrumb-item>
     </el-breadcrumb>
     <el-card>
-      <el-row :gutter="10">
-        <el-col :span="7">
+      <el-row :gutter="20">
+        <el-col :span="10">
           <el-input placeholder="请输入内容" v-model="keywords" clearable @clear="getUserList">
             <el-button slot="append" icon="el-icon-search" @click="searchUser(keywords)"></el-button>
           </el-input>
         </el-col>
-        <el-col :span="2">
+        <el-col :span="3">
           <el-button type="primary" @click="addDialogVisible = true">添加用户</el-button>
         </el-col>
         <el-col :span="3">
