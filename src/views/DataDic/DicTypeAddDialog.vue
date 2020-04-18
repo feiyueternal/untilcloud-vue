452930187cb62ba92b7f6ec327742543304513e4
lanlin <1094506821@qq.com>
2020-04-18 10:22:23 +0800

    增加字典类型的同时还可以增加明细

--- src/views/DataDic/DicTypeAddDialog.vue
+++ src/views/DataDic/DicTypeAddDialog.vue
@@ -23,6 +23,35 @@
         </el-col>
       </el-row>
     </el-form>
+    <el-form :model="addDetailForm" ref="addDetailForm" label-width="80px">
+      <el-row>
+        <el-button type="warning" size="mini" @click="addNewDetail()">增加明细</el-button>
+        <el-button type="danger" size="mini" @click="delNewDetail()">删除明细</el-button>
+      </el-row>
+      <div>
+        <el-row v-for="(item, id) in addDetailForm.detailArray" :key="id">
+          {{'明细' + (id+1) + ':'}}
+          <el-form-item label="名称">
+            <el-input v-model="item.name" placeholder="请输入名称"></el-input>
+          </el-form-item>
+          <el-form-item label="排序">
+            <el-input v-model="item.sort" placeholder="请输入排序"></el-input>
+          </el-form-item>
+          <el-form-item label="值">
+            <el-input v-model="item.value" placeholder="请输入值"></el-input>
+          </el-form-item>
+          <el-row>
+            <el-col>
+              <span>状态:</span>
+            </el-col>
+            <el-col>
+              <el-radio v-model="item.status" label="true">正常</el-radio>
+              <el-radio v-model="item.status" label="false">禁用</el-radio>
+            </el-col>
+          </el-row>
+        </el-row>
+      </div>
+    </el-form>
     <span slot="footer" class="dialog-footer">
       <el-button @click="addDialogVisible = false">取 消</el-button>
       <el-button type="primary" @click="addDicType">确 定</el-button>
@@ -41,30 +70,48 @@ export default {
         name: "",
         status: "true"
       },
+      addDetailForm: { detailArray: [] },
       rules: {
         name: [
           { required: true, message: "请输入类型中文说明", trigger: "blur" }
         ],
         code: [{ required: true, message: "请输入类型编码", trigger: "blur" }]
       }
+      // DetailRules: {
+      //   name: [{ required: true, message: "请输入名称", trigger: "blur" }],
+      //   sort: [{ required: true, message: "请输入排序", trigger: "blur" }],
+      //   value: [{ required: true, message: "请输入值", trigger: "blur" }]
+      // }
     };
   },
   methods: {
     addDicType() {
       this.$refs.addDialogForm.validate(valid => {
         if (valid) {
-          var url = "/index/sys/dic/type/add";
+          var detailList = [];
+          if (this.addDetailForm.detailArray.length == 0) {
+            var detailList = [];
+          } else {
+            for (var i = 0; i < this.addDetailForm.detailArray.length; i++) {
+              detailList.push(this.addDetailForm.detailArray[i]);
+            }
+          }
+
+          var url = "/index/sys/dic/type/adds";
           var data = {
-            code: this.addDialogForm.code,
-            name: this.addDialogForm.name,
-            status: this.addDialogForm.status
+            dictionaryType: {
+              code: this.addDialogForm.code,
+              name: this.addDialogForm.name,
+              status: this.addDialogForm.status
+            },
+            dictionaryInfos: detailList
           };
           //   console.log(data)
           this.$http
             .post(url, data)
             .then(res => {
               if (res.data.code == 200) {
-                this.$message.success("添加菜单成功");
+                this.$message.success("添加字典类型成功");
                 this.$emit("needfresh", true);
                 this.addDialogVisible = false;
               } else {
@@ -81,12 +128,35 @@ export default {
     },
     addDialogClosed() {
       this.$refs.addDialogForm.resetFields();
+      this.addDetailForm.detailArray = [];
     },
     open() {
       this.addDialogVisible = true;
+    },
+    addNewDetail() {
+      var infos = {
+        sort: "",
+        name: "",
+        value: "",
+        status: "true"
+      };
+      this.addDetailForm.detailArray.push(infos);
+      console.log(this.addDetailForm);
+    },
+    delNewDetail() {
+      if (this.addDetailForm.detailArray.length == 0) {
+      } else {
+        this.addDetailForm.detailArray.pop();
+      }
     }
   }
 };
 </script>
 <style scoped>
+.el-button {
+  margin-top: 15px;
+}
+.el-row {
+  margin-top: 15px;
+}
 </style>
